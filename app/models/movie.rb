4c39a65fe530056ff96eb97ca30542b0e6e080f7
Jordan Hancock <jhancock@chicagobooth.edu>
2023-05-04 15:10:44 +0000

    Associations mostly working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,13 +13,16 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-  def director
-    key = self.director_id
 
-    matching_set = Director.where({ :id => key })
+  belongs_to(:director, {:foreign_key => "director_id", :class_name => "Director"})
+  has_many(:characters, {:foreign_key => "movie_id", :class_name => "Character"})
+  # def director
+  #   key = self.director_id
 
-    the_one = matching_set.at(0)
+  #   matching_set = Director.where({ :id => key })
 
-    return the_one
-  end
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
